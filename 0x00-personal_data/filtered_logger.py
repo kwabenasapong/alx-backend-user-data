aa84ce3972972180c7348ee20e5da13697253910
Kwabena Sapong <kwabenasapong@gmail.com>
2023-04-27 16:14:57 +0000

    Solve task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,3 +19,23 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        """ Redacting Formatter class
+            """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ filter values in incoming log records using filter_datum
+            """
+        return filter_datum(PII_FIELDS, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
