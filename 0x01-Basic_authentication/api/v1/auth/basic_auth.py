483ed08de5bfe3dfa74c2b1509c2ec27999f1c77
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-03 09:57:29 +0000

    Update basic_auth for task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -65,3 +65,11 @@ class BasicAuth(Auth):
             if user.is_valid_password(user_pwd):
                 return user
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Overload current_user"""
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        decoded_header = self.decode_base64_authorization_header(base64_header)
+        user, pwd = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(user, pwd)
