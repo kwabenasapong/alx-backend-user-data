ac7b59e19b6843c3a05ba49f6fa5f81643497d0b
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-03 08:18:30 +0000

    Update app and index with 403 error handling

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def status_unauthorized() -> str:
       - raises a 401 error by using abort
     """
     return abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def status_forbidden() -> str:
+    """GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
+    """
+    return abort(403)
