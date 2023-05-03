165aa67456e76820ce55445575c6611546c1b488
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-03 08:54:57 +0000

    Add auth related dir and files

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth method
+        """
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header method
+        """
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user method
+        """
+        return None
