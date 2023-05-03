6230e186be1e5af788afca993ee8161ce3db5a17
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-03 09:44:10 +0000

    Update basic_auth for task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 
 from api.v1.auth.auth import Auth
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -47,3 +48,17 @@ class BasicAuth(Auth):
             return None, None
         user_pass = decoded_base64_authorization_header.split(':', 1)
         return user_pass[0], user_pass[1]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """user_object_from_credentials method"""
+        if user_email is None or user_pwd is None:
+            return None
+        if type(user_email) is not str or type(user_pwd) is not str:
+            return None
+        from models.user import User
+        users = User.search({'email': user_email})
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
