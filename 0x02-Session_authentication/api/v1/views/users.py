6a8bedf11602584deb48369f815f0b0d407ffa7b
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-04 07:57:48 +0000

    Update users.py in views

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -32,7 +32,7 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
     if user_id == me and request.current_user is None:
         abort(404)
-    if user_id == me and request.current_user.id != None:
+    if user_id == me and request.current_user.id is not None:
         return jsonify(request.current_user.to_json())
     return jsonify(user.to_json())
 
