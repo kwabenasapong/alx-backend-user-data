2aecdcad372843a030ca4158a2cf4aeafcab0fad
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-08 10:33:33 +0000

    Update & add to views dir __init__ & session_auth

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+""" SessionAuth Module for Views"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """Create a new session for the user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == "":
+        return jsonify({"error": "password missing"}), 400
+    try:
+        user = User.search({'email': email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    for u in user:
+        if not u.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].id)
+    response = jsonify(user[0].to_json())
+    response.set_cookie(getenv('SESSION_NAME'), session_id)
+    return response
