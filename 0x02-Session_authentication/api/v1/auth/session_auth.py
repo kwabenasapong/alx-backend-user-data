d5fb8030f606ccfa1a8a080af3e1b2f704ecfdf2
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-08 09:01:42 +0000

    Update session_auth for task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,8 +14,15 @@ class SessionAuth(Auth):
         """Creates a Session ID for a user_id
         """
         from uuid import uuid4
-        if user_id is None or type(user_id) != str:
+        if user_id is None or type(user_id) is not str:
             return None
         session_id = str(uuid4())
         SessionAuth().user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Return user id based on session id
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return SessionAuth().user_id_by_session_id.get(session_id)
