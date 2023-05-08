e7fa21b489e24b406d63701c629359fdaf37116c
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-08 10:15:06 +0000

    Update session_auth for task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,11 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return SessionAuth().user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Return user instance based on cookie value
+        """
+        from models.user import User
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        return User.get(user_id)
