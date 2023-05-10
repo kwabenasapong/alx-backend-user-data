541519e211b742a35946a712b30dc15c1b0c9c46
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-10 11:00:08 +0000

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -36,4 +38,18 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Takes in arbitrary keyword arguments and returns the first row
+        found in the users table as filtered by the method’s input arguments
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        # if not all([hasattr(User, key) for key in kwargs]):
+        #     raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
         return user
