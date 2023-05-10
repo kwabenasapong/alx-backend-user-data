6ff4e889c929259f3fcba8fc6b4a58265c7daebf
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-10 11:27:23 +0000

    Update appy

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -27,5 +27,18 @@ def basic_route() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'])
+def users() -> str:
+    """Users route
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = auth.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
