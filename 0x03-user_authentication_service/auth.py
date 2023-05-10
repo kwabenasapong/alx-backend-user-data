e812ae0b3e4453a80c9596ca08355a7f05ca17a7
Kwabena Sapong <kwabenasapong@gmail.com>
2023-05-10 11:09:47 +0000

    Add auth

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Auth Module
+In this task you will define
+a _hash_password method that takes in a
+password string arguments and returns bytes.
+
+The returned bytes is a salted hash of the
+input password, hashed with bcrypt.hashpw.
+"""
+
+from bcrypt import hashpw, gensalt, checkpw
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
+
+
+def _hash_password(password: str) -> str:
+    """Hash password
+    """
+    return hashpw(password.encode('utf-8'), gensalt())
